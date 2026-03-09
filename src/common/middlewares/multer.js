c1c75e64ebb8f4c54ba1f6e8874230f279550b32
Ahmed Mostafa <ahmedmostafa12.com@gmail.com>
2026-03-09 11:09:18 +0200

    add file filter function and add new type of extensions

--- src/common/middlewares/multer.js
+++ src/common/middlewares/multer.js
@@ -1,8 +1,13 @@
 import multer from "multer";
 import fs from "fs"
 
-
-export const multer_local = ({ customPath } = { customPath: 'genaral' }) => {
+export let extensions = {
+    image: ['image/jpeg', 'image/png', 'image/jpg', 'image/jpeg', 'image/gif', 'image/webp'],
+    video: ['video/mp4', 'video/avi', 'video/mkv', 'video/mov', 'video/wmv'],
+    audio: ['audio/mp3', 'audio/wav', 'audio/aac', 'audio/flac', 'audio/ogg'],
+    document: ['application/pdf']
+}
+export const multer_local = ({ customPath, allowedExtensions } = { customPath: 'genaral' }) => {
     let storage = multer.diskStorage({
         destination: function (req, file, cb) {
             let path = `./uploads/${customPath}`
@@ -16,5 +21,11 @@ export const multer_local = ({ customPath } = { customPath: 'genaral' }) => {
             cb(null, prefix)
         }
     })
-    return multer({ storage })
+    let fileFilter = function (req, file, cb) {
+        if (!allowedExtensions.includes(file.mimetype)) {
+            cb(new Error('Invalid file type'), false);
+        }
+        cb(null, true)
+    }
+    return multer({ storage, fileFilter })
 }
